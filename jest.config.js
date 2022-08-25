d1c475503fbf9604fbf1861cf193ad72b318da16
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-08-25 17:12:54 -0400

    update new format

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    // "^.+\\.ts?$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest", // use either first or second
   },
   testEnvironment: "node",
-  testRegex: "./src/.*\\.(test|spec)?\\.(ts|ts)$",
+  // testRegex: "./src/.*\\.(test|spec)?\\.(ts|ts)$",
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   roots: ["<rootDir>/src"],
 };
